70051a56320eae3c2d03f7feda03ae1312f8a310
VS <vaish261@gmail.com>
2025-07-27 22:42:54 +0200

    section definition

--- src/App.js
+++ src/App.js
@@ -4,11 +4,41 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className="section1">
+        <div className= "Top">
+
+        </div>
+        <div className="banner">
+
+        </div>
+      </div>
+
+      <div className='howitworks'>
+        <div className='step1'></div>
+        <div className='step2'></div>
+        <div className='step3'></div>
+      </div>
+
+      <div className='programs'>
+        <div className=''></div>
+        <div className=''></div>
+        <div className=''></div>
+        <div className=''></div>
+        <div className=''></div>
+        <div className=''></div>
+        
+      </div>
+
+      <div className='pricing'>
+        <div className='t1'></div>
+        <div className='t2'></div>
+        <div className='t3'></div>
+      </div>
+
+      <div className='contactus'>
+        <div className='contact'></div>
+      </div>
+
     </div>
   );
 }
